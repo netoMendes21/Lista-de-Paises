6ed6ac602e9ae170a01c5501e1c79c98ae8bf70c
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-07-02 20:32:54 -0300

    botão de voltar adicionado e funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-type Country = {
+export type Country = {
   name: {
     common: string
   }
--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -1,3 +1,26 @@
-export default function Country() {
-  return <h1>Country</h1>
+import type { Country } from "@/app/page" 
+import Image from "next/image"
+import Link from "next/link"
+
+async function getCountryByName(name: string): Promise<Country> {
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+  return (await response.json())[0] 
+}
+
+export default async function CountryPage({params: {name}}: {params: {name: string}}) {
+
+  const country = await getCountryByName(name)
+  return (
+    <section className="flex flex-col container">
+
+      <h1 className="text-5xl text-center font-bold text-gray-800 my-16">{country.translations.por.common}</h1>
+      <Link href="/" className="flex items-center py-2">
+      <Image src="/arrow-back.svg" alt="seta de voltar" width={24} height={24}/>
+      Voltar
+      </Link>
+      <article className="flex justify-between min-w-full p-10 bg-white rounded-xl">
+        <Image src={country.flags.svg} alt={country.flags.alt}/>
+      </article>
+    </section>
+)
 }
