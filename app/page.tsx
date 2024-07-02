60fb09d4b790946a8dda5e68bbdad35a89f9e443
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-07-02 19:34:08 -0300

    criando a página de país

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 type Country = {
   name: {
@@ -28,12 +29,14 @@ export default async function Home() {
   return (
   <section className="grid grid-cols-5 w-full container gap-2 mt-16">
     {countries.map((country) => (
-      <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200" key={country.name.common}>
+      <Link href={`/pais/${country.name.common}`} key={country.translations.por.common}>
+      <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl" key={country.name.common}>
         <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
         <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover"/>
         </div>
-        <h1>{country.translations.por.common}</h1>
+        <h1 className="font-bold text-xl text-center mt-5">{country.translations.por.common}</h1>
         </article>
+      </Link>
         ))}
         </section>
   );
