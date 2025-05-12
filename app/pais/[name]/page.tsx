5cdeb5b62a0b6aa58d33ad9f29c04fc04ce49352
José Luiz Neto <133919788+netoMendes21@users.noreply.github.com>
2025-05-12 11:29:54 -0300

    Update page.tsx

--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -51,7 +51,7 @@ export default async function CountryPage({params: {name}}: {params: {name: stri
           <b>🏙️ Capital:</b> {country.capital}
         </h2>)}
         <h2 className="text-xl text-gray-800">
-          <b>🗺️ Continente: Europe</b> {country.region}{country.subregion && `- ${country.subregion}`}
+          <b>🗺️ Continente: {country.region}{country.subregion && `- ${country.subregion}`}
         </h2>
         <h2 className="text-xl text-gray-800">
           <b>👨‍👩‍👧‍👦 População:</b> {formatter.format(country.population)}
